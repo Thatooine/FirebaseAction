bb6c4ab0a86da7ad1ca3339c7924a5c7a49d50f3
Thato <tmthatoo4@gmail.com>
2019-09-20 16:08:44 +0200

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
-cd ./src
-echo $GITHUB_REPOSITORY
-cat index.tsx
+# cd ./src
+# echo $GITHUB_REPOSITORY
+# cat index.tsx
+npm react-scripts build
+npm install -g firebase-tools
+firebase deploy --token $INPUT_FIREBASE_TOKEN
